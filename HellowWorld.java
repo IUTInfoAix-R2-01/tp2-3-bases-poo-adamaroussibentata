d3eba371174e3abe6d24e2960dff0a1ba16d909a
adamaroussi67+1@gmail.com <adam.AROUSSI-BENTATA@etu.univ-amu.fr>
2025-01-28 14:52:33 +0100

    modification du HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,5 +1,7 @@
 package HelloWorld;
 
 public class HellowWorld {
-
-}
+    public static void main(String[] args) {
+        // Affiche le message "Hello, World!" dans la console
+        System.out.println("Hello, World!");
+    }
